440dca77b8f83986a18f08ead43130cd8f493eab
anjolaoluwaakindipe <anjyakindipe@gmail.com>
2023-10-15 12:00:32 -0400

    Started gateway service

--- internal/routing/router.go
+++ internal/routing/router.go
@@ -2,7 +2,10 @@ package routing
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/gorilla/mux"
 )
@@ -17,15 +20,38 @@ type MuxRouter struct {
 } 
 
 func (mr *MuxRouter) RegisterRoutes() {
-	mr.router.HandleFunc("/{path}",mr.GetPath()).Methods("GET")
+	mr.router.HandleFunc("/{path}",mr.GetPath(mr.ProxyRequest)).Methods("GET")
 	mr.router.Use(mux.CORSMethodMiddleware(mr.router))
 }
 
-func (mr *MuxRouter) GetPath() func(http.ResponseWriter, *http.Request) {
+func (mr *MuxRouter) GetPath(proxyfunc func (string )(*httputil.ReverseProxy, error)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := mux.Vars(r)
 		fmt.Println(path)
+		//  use path to get address from service discovery
+		address := "asdf"
+
+		proxy, err := proxyfunc(address)
+		if err != nil {
+			log.Printf("address of discovered service is invalid : %v", err)
+			return 
+		}
+
+		proxy.ServeHTTP(w, r)
+
 	}
+}
+
+func (mr *MuxRouter) GetAddress(address string) (string, error) {
+	return "", nil 
+}
+
+func (mr *MuxRouter) ProxyRequest(targetUrl string) (*httputil.ReverseProxy, error){
+	url , err := url.Parse(targetUrl)
+	if err != nil {
+		return nil, err
+	}
+	return httputil.NewSingleHostReverseProxy(url), nil
 }
 
 func (mr *MuxRouter) GetRouter() http.Handler {
